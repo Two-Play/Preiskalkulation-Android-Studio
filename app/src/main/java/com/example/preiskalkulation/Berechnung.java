52518300e4ecd9095c8ef59a23c78f3468e0f39a
Philippe Westenfelder <phinfo19@gmail.com>
2022-05-16 13:59:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/preiskalkulation/Berechnung.java
@@ -0,0 +1,345 @@
+package com.example.preiskalkulation;
+
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Berechnung {
+    private boolean vorwaerts;
+    private boolean rueckwaerts;
+    private boolean differenz;
+    private double materialeinzelkosten;
+    private double materialgemeinkosten;
+    private double ergebnisMGK;
+    private double materialkosten;
+    private double fertigungseinzelkosten;
+    private double fertigungsgemeinkosten;
+    private double ergebnisFGK;
+    private double sonderEkdFert;
+    private double fertigungskosten;
+    private double herstellkosten;
+    private double verwaltungsgemeinkosten;
+    private double vertriebsgemeinkosten;
+    private double ergebnisvertGK;
+    private double ergebnisVerwGK;
+    private double sondereinzelkosten_des_Vertriebs;
+    private double selbstkosten;
+    private double gewinn;
+    private double ergebnisGewinn;
+    private double barverkaufspreis;
+    private double kundenskonto;
+    private double ergebnisKs;
+    private double zielverkaufspreis;
+    private double kundenrabatt;
+    private double ergebnisKr;
+    private double nettoverkaufspreis;
+    private double umsatzsteuer;
+    private double ergebnisUmst;
+    private double bruttoverkaufspreis;
+
+
+    //setter und getter
+
+
+    public double getErgebnisMGK() {
+        return ergebnisMGK;
+    }
+
+    public void setErgebnisMGK(double ergebnisMGK) {
+        this.ergebnisMGK = ergebnisMGK;
+    }
+
+    public double getErgebnisFGK() {
+        return ergebnisFGK;
+    }
+
+    public void setErgebnisFGK(double ergebnisFGK) {
+        this.ergebnisFGK = ergebnisFGK;
+    }
+
+    public double getSonderEkdFert() {
+        return sonderEkdFert;
+    }
+
+    public void setSonderEkdFert(double sonderEkdFert) {
+        this.sonderEkdFert = sonderEkdFert;
+    }
+
+    public double getVertriebsgemeinkosten() {
+        return vertriebsgemeinkosten;
+    }
+
+    public void setVertriebsgemeinkosten(double vertriebsgemeinkosten) {
+        this.vertriebsgemeinkosten = vertriebsgemeinkosten;
+    }
+
+    public double getErgebnisvertGK() {
+        return ergebnisvertGK;
+    }
+
+    public void setErgebnisvertGK(double ergebnisvertGK) {
+        this.ergebnisvertGK = ergebnisvertGK;
+    }
+
+    public double getErgebnisVerwGK() {
+        return ergebnisVerwGK;
+    }
+
+    public void setErgebnisVerwGK(double ergebnisVerwGK) {
+        this.ergebnisVerwGK = ergebnisVerwGK;
+    }
+
+    public double getErgebnisGewinn() {
+        return ergebnisGewinn;
+    }
+
+    public void setErgebnisGewinn(double ergebnisGewinn) {
+        this.ergebnisGewinn = ergebnisGewinn;
+    }
+
+    public double getErgebnisKs() {
+        return ergebnisKs;
+    }
+
+    public void setErgebnisKs(double ergebnisKs) {
+        this.ergebnisKs = ergebnisKs;
+    }
+
+    public double getErgebnisKr() {
+        return ergebnisKr;
+    }
+
+    public void setErgebnisKr(double ergebnisKr) {
+        this.ergebnisKr = ergebnisKr;
+    }
+
+    public double getErgebnisUmst() {
+        return ergebnisUmst;
+    }
+
+    public void setErgebnisUmst(double ergebnisUmst) {
+        this.ergebnisUmst = ergebnisUmst;
+    }
+
+    public double getVerwaltungsgemeinkosten() {
+        return verwaltungsgemeinkosten;
+    }
+
+    public void setVerwaltungsgemeinkosten(double verwaltungsgemeinkosten) {
+        this.verwaltungsgemeinkosten = verwaltungsgemeinkosten;
+    }
+
+    public double getSondereinzelkosten_des_Vertriebs() {
+        return sondereinzelkosten_des_Vertriebs;
+    }
+
+    public void setSondereinzelkosten_des_Vertriebs(double sondereinzelkosten_des_Vertriebs) {
+        this.sondereinzelkosten_des_Vertriebs = sondereinzelkosten_des_Vertriebs;
+    }
+
+    public double getSelbstkosten() {
+        return selbstkosten;
+    }
+
+    public void setSelbstkosten(double selbstkosten) {
+        this.selbstkosten = selbstkosten;
+    }
+
+    public double getGewinn() {
+        return gewinn;
+    }
+
+    public void setGewinn(double gewinn) {
+        this.gewinn = gewinn;
+    }
+
+    public double getBarverkaufspreis() {
+        return barverkaufspreis;
+    }
+
+    public void setBarverkaufspreis(double barverkaufspreis) {
+        this.barverkaufspreis = barverkaufspreis;
+    }
+
+    public double getSonderEkdVert() {
+        return sonderEkdFert;
+    }
+
+    public void setSonderEkdVert(double sonderEkdVert) {
+        this.sonderEkdFert = sonderEkdVert;
+    }
+
+    public double getKundenskonto() {
+        return kundenskonto;
+    }
+
+    public void setKundenskonto(double kundenskonto) {
+        this.kundenskonto = kundenskonto;
+    }
+
+    public double getZielverkaufspreis() {
+        return zielverkaufspreis;
+    }
+
+    public void setZielverkaufspreis(double zielverkaufspreis) {
+        this.zielverkaufspreis = zielverkaufspreis;
+    }
+
+    public double getKundenrabatt() {
+        return kundenrabatt;
+    }
+
+    public void setKundenrabatt(double kundenrabatt) {
+        this.kundenrabatt = kundenrabatt;
+    }
+
+    public double getNettoverkaufspreis() {
+        return nettoverkaufspreis;
+    }
+
+    public void setNettoverkaufspreis(double nettoverkaufspreis) {
+        this.nettoverkaufspreis = nettoverkaufspreis;
+    }
+
+    public double getUmsatzsteuer() {
+        return umsatzsteuer;
+    }
+
+    public void setUmsatzsteuer(double umsatzsteuer) {
+        this.umsatzsteuer = umsatzsteuer;
+    }
+
+    public double getBruttoverkaufspreis() {
+        return bruttoverkaufspreis;
+    }
+
+    public void setBruttoverkaufspreis(double bruttoverkaufspreis) {
+        this.bruttoverkaufspreis = bruttoverkaufspreis;
+    }
+
+    public boolean isVorwaerts() {
+        return vorwaerts;
+    }
+
+    public void setVorwaerts(boolean vorwaerts) {
+        this.vorwaerts = vorwaerts;
+    }
+
+    public boolean isRueckwaerts() {
+        return rueckwaerts;
+    }
+
+    public void setRueckwaerts(boolean rueckwaerts) {
+        this.rueckwaerts = rueckwaerts;
+    }
+
+    public boolean isDifferenz() {
+        return differenz;
+    }
+
+    public void setDifferenz(boolean differenz) {
+        this.differenz = differenz;
+    }
+
+    public double getMaterialeinzelkosten() {
+        return materialeinzelkosten;
+    }
+
+    public void setMaterialeinzelkosten(double materialeinzelkosten) {
+        this.materialeinzelkosten = materialeinzelkosten;
+    }
+
+    public double getMaterialgemeinkosten() {
+        return materialgemeinkosten;
+    }
+
+    public void setMaterialgemeinkosten(double materialgemeinkosten) {
+        this.materialgemeinkosten = materialgemeinkosten;
+    }
+
+    public double getMaterialkosten() {
+        return materialkosten;
+    }
+
+    public void setMaterialkosten(double materialkosten) {
+        this.materialkosten = materialkosten;
+    }
+
+    public double getFertigungseinzelkosten() {
+        return fertigungseinzelkosten;
+    }
+
+    public void setFertigungseinzelkosten(double fertigungseinzelkosten) {
+        this.fertigungseinzelkosten = fertigungseinzelkosten;
+    }
+
+    public double getFertigungsgemeinkosten() {
+        return fertigungsgemeinkosten;
+    }
+
+    public void setFertigungsgemeinkosten(double fertigungsgemeinkosten) {
+        this.fertigungsgemeinkosten = fertigungsgemeinkosten;
+    }
+
+    public double getFertigungskosten() {
+        return fertigungskosten;
+    }
+
+    public void setFertigungskosten(double fertigungskosten) {
+        this.fertigungskosten = fertigungskosten;
+    }
+
+    public double getHerstellkosten() {
+        return herstellkosten;
+    }
+
+    public void setHerstellkosten(double herstellkosten) {
+        this.herstellkosten = herstellkosten;
+    }
+
+
+
+    //Eigene Methoden
+    private double prozentBerechnen(double prozentzsatz, double grundwert){
+        return prozentzsatz/100*grundwert;
+    }
+
+    public double convertToDouble(EditText textField){
+        String value = textField.getText().toString();
+        //Überprüfung ob EditText leer ist
+        if (!value.matches("")){
+            return Double.parseDouble(textField.getText().toString());
+        }else {
+            Log.i("INFO", "Set value to 0.0");
+            return 0.0;
+        }
+    }
+
+    public String convertToString(double number){
+        return String.valueOf(String.format("%.2f", number)+" €");
+    }
+
+    public void vorwaertsBerechnung(){
+        setErgebnisMGK(prozentBerechnen(getMaterialgemeinkosten(),getMaterialeinzelkosten()));
+        setMaterialkosten((getErgebnisMGK()+ getMaterialeinzelkosten()));
+        setErgebnisFGK(prozentBerechnen(getFertigungsgemeinkosten(),getFertigungseinzelkosten()));
+        setFertigungskosten(getErgebnisFGK()+ getFertigungseinzelkosten()+getSonderEkdVert());
+        setHerstellkosten(getMaterialkosten()+getFertigungskosten());
+        setErgebnisVerwGK(prozentBerechnen(getVerwaltungsgemeinkosten(),getHerstellkosten()));
+        setErgebnisvertGK(prozentBerechnen(getVertriebsgemeinkosten(),getHerstellkosten()));
+        setSelbstkosten(getSonderEkdFert()+getErgebnisVerwGK()+getErgebnisvertGK()+getHerstellkosten());
+        setErgebnisGewinn(prozentBerechnen(getGewinn(),getSelbstkosten()));
+        setBarverkaufspreis(getSelbstkosten()+getErgebnisGewinn());
+        double prozent_im_hundert = 100-getKundenskonto();
+        setErgebnisKs(getBarverkaufspreis()/prozent_im_hundert*getKundenskonto());
+        setZielverkaufspreis(getBarverkaufspreis()+getErgebnisKs());
+        prozent_im_hundert = 100-getKundenrabatt();
+        setErgebnisKr(getZielverkaufspreis()/prozent_im_hundert*getKundenrabatt());
+        setNettoverkaufspreis(getZielverkaufspreis()+getErgebnisKr());
+        setErgebnisUmst(prozentBerechnen(getUmsatzsteuer(),getNettoverkaufspreis()));
+        setBruttoverkaufspreis(getNettoverkaufspreis()+getErgebnisUmst());
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/preiskalkulation/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.preiskalkulation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MaterialButtonToggleGroup materialButtonToggleGroup = findViewById(R.id.toggleButton);
+
+        Button berechnung = findViewById(R.id.berechnung);
+        EditText mek = findViewById(R.id.mekTF);
+        EditText mgk = findViewById(R.id.mgkTF);
+        TextView mk = findViewById(R.id.ergebnisMK);
+        EditText fek = findViewById(R.id.fekTF);
+        EditText fgk = findViewById(R.id. fgkTF);
+        EditText sdFert = findViewById(R.id.sdFertTF);
+        TextView fk = findViewById(R.id.ergebnisFK);
+        TextView hk = findViewById(R.id.herstellkosten);
+        EditText verwGk = findViewById(R.id.verwGkTF);
+        EditText vertGk = findViewById(R.id.vertTF);
+        EditText sdVert = findViewById(R.id.sdvTF);
+        TextView sk = findViewById(R.id.ergebnisSK);
+        EditText gewin = findViewById(R.id.gewinnTF);
+        TextView bvpTV = findViewById(R.id.ergebnisBVP);
+        EditText ks = findViewById(R.id.ksTF);
+        TextView zvp = findViewById(R.id.ergebnisZVP);
+        EditText kr = findViewById(R.id.krTF);
+        EditText nvp = findViewById(R.id.nvpTF);
+        EditText umst = findViewById(R.id.umstTF);
+        EditText bvp = findViewById(R.id.bvpTF);
+        Button loeschenBtn = findViewById(R.id.loeschen);
+        TextView ergebnisMGK = findViewById(R.id.ergebnisMGK);
+        TextView ergebnisFGK = findViewById(R.id.ergebnisFGK);
+        TextView ergebnisVerwGK = findViewById(R.id.ergebnisVerwGK);
+        TextView ergebnisVertGk = findViewById(R.id.ergebnisverTGK);
+        TextView ergebnisGewinn = findViewById(R.id.ergebnisGewinn);
+        TextView ergebnisKr = findViewById(R.id.ergebnisKr);
+        TextView ergebnisKs = findViewById(R.id.ergebnisKs);
+        TextView ergebnisUmst = findViewById(R.id.ergebnisUmst);
+
+        Berechnung objBerechnung = new Berechnung();
+        objBerechnung.setVorwaerts(true);
+        berechnung.setEnabled(true);
+        nvp.setEnabled(false);
+        bvp.setEnabled(false);
+        //Button GroupActionListener
+        materialButtonToggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+                if (isChecked) {
+                    //Button ID check
+
+                    switch (checkedId){
+                        case R.id.vorwaertsBtn:
+                            Log.i("DEBUG", "Vorwärtskalk");
+                            objBerechnung.setVorwaerts(true);
+                            objBerechnung.setRueckwaerts(false);
+                            objBerechnung.setDifferenz(false);
+
+                            gewin.setEnabled(true);
+                            nvp.setEnabled(false);
+                            bvp.setEnabled(false);
+                            nvp.setText(null);
+                            bvp.setText(null);
+                            break;
+                        case R.id.rueckwaertsBtn:
+                            Log.i("DEBUG", "Rückwärtskalk");
+                            objBerechnung.setVorwaerts(false);
+                            objBerechnung.setRueckwaerts(true);
+                            objBerechnung.setDifferenz(false);
+
+                            gewin.setEnabled(true);
+                            nvp.setEnabled(true);
+                            bvp.setEnabled(true);
+
+                            break;
+                        case R.id.differenzBtn:
+                            Log.i("DEBUG", "Differenzkalk");
+                            objBerechnung.setVorwaerts(false);
+                            objBerechnung.setRueckwaerts(false);
+                            objBerechnung.setDifferenz(true);
+
+                            gewin.setEnabled(false);
+                            nvp.setEnabled(true);
+                            bvp.setEnabled(true);
+                            gewin.setText(null);
+                            break;
+                        default:
+                            Log.e("ERROR", "Case nicht gefunden");
+                            break;
+
+                    }
+                }
+            }
+        });
+
+        berechnung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                objBerechnung.setMaterialeinzelkosten(objBerechnung.convertToDouble(mek));
+                objBerechnung.setMaterialgemeinkosten((objBerechnung.convertToDouble(mgk)));
+                objBerechnung.setFertigungseinzelkosten(objBerechnung.convertToDouble(fek));
+                objBerechnung.setFertigungsgemeinkosten((objBerechnung.convertToDouble(fgk)));
+                objBerechnung.setSonderEkdVert(objBerechnung.convertToDouble(sdVert));
+                objBerechnung.setVerwaltungsgemeinkosten(objBerechnung.convertToDouble(verwGk));
+                objBerechnung.setVertriebsgemeinkosten(objBerechnung.convertToDouble(vertGk));
+                objBerechnung.setGewinn(objBerechnung.convertToDouble(gewin));
+                objBerechnung.setKundenskonto(objBerechnung.convertToDouble(ks));
+                objBerechnung.setKundenrabatt(objBerechnung.convertToDouble(kr));
+                objBerechnung.setUmsatzsteuer(objBerechnung.convertToDouble(umst));
+                //df
+
+                if (objBerechnung.isVorwaerts()){
+                    objBerechnung.vorwaertsBerechnung();
+                }
+
+                ergebnisMGK.setText(objBerechnung.convertToString(objBerechnung.getErgebnisMGK()));
+                mk.setText(getString(R.string.ergebnisMaterialkosten) + " " + objBerechnung.convertToString(objBerechnung.getMaterialkosten()));
+                fk.setText(getString(R.string.ergebnisFertigungskosten) + " " + objBerechnung.convertToString(objBerechnung.getFertigungskosten()));
+                ergebnisFGK.setText(objBerechnung.convertToString(objBerechnung.getErgebnisFGK()));
+                hk.setText(getString(R.string.herstellkosten) + " " + objBerechnung.convertToString(objBerechnung.getHerstellkosten()));
+
+                ergebnisVerwGK.setText(objBerechnung.convertToString(objBerechnung.getErgebnisVerwGK()));
+                ergebnisVertGk.setText(objBerechnung.convertToString(objBerechnung.getErgebnisvertGK()));
+                sk.setText(getString(R.string.ergebnisSelbskosten) + " " + objBerechnung.convertToString(objBerechnung.getSelbstkosten()));
+
+                ergebnisGewinn.setText(objBerechnung.convertToString(objBerechnung.getErgebnisGewinn()));
+                bvpTV.setText(getString(R.string.ergebnisBarverkaufspreis) + " " + objBerechnung.convertToString(objBerechnung.getBarverkaufspreis()));
+                ergebnisKs.setText(objBerechnung.convertToString(objBerechnung.getErgebnisKs()));
+                zvp.setText(String.format("%s %s", getString(R.string.ergebnisZielverkaufspreis), objBerechnung.convertToString(objBerechnung.getZielverkaufspreis())));
+                ergebnisKr.setText(objBerechnung.convertToString(objBerechnung.getErgebnisKr()));
+
+                nvp.setText(objBerechnung.convertToString(objBerechnung.getNettoverkaufspreis()));
+                ergebnisUmst.setText(objBerechnung.convertToString(objBerechnung.getErgebnisUmst()));
+                bvp.setText(objBerechnung.convertToString(objBerechnung.getBruttoverkaufspreis()));
+
+            }
+        });
+
+        loeschenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mek.setText(null);
+                mgk.setText(null);
+                mk.setText(R.string.ergebnisMaterialkosten);
+                ergebnisMGK.setText(null);
+                fek.setText(null);
+                fgk.setText(null);
+                sdFert.setText(null);
+                ergebnisFGK.setText(null);
+                fk.setText(R.string.ergebnisFertigungskosten);
+                hk.setText(R.string.herstellkosten);
+                vertGk.setText(null);
+                ergebnisVertGk.setText(null);
+                verwGk.setText(null);
+                ergebnisVerwGK.setText(null);
+                sdVert.setText(null);
+                sk.setText(R.string.ergebnisSelbskosten);
+                gewin.setText(null);
+                bvpTV.setText(R.string.ergebnisBarverkaufspreis);
+                ks.setText(null);
+                zvp.setText(R.string.ergebnisZielverkaufspreis);
+                kr.setText(null);
+                nvp.setText(null);
+                umst.setText(null);
+                bvp.setText(null);
+                ergebnisGewinn.setText(null);
+                ergebnisKr.setText(null);
+                ergebnisKs.setText(null);
+                ergebnisUmst.setText(null);
+                Toast.makeText(MainActivity.this,"Alle Daten gelöscht",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
